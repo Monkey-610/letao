449508fee8914e7473a9155693de5cd896581009
Monkey-610 <1947743548@qq.com>
2018-12-22 15:12:25 +0800

    首页完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -9,6 +9,7 @@
 /*
  * 
  *
+ * 
  * ajax全局事件
  *    .ajaxComplete()  当每个ajax完成时,调用     (不管成功还是失败)
  *    .ajaxSuccess()   当ajax返回成功时调用
@@ -21,17 +22,50 @@
 
 /* 需求: 在第一个ajax发送的时候, 开启进度条
    在全部的ajax回来的时候, 关闭进度条 */
-   $(function () {
-     $(document).ajaxStart(function () {
-       NProgress.start()
-     });
-     /* $(document).ajaxStart(function () {
-       NProgress.set(0.5)
-     }); */
-     $(document).ajaxStop(function () {
-       setTimeout(function () {
-        NProgress.done()
-       },1000)
-     });
+$(function () {
+  $(document).ajaxStart(function () {
+    NProgress.start()
+  });
+  /* $(document).ajaxStart(function () {
+    NProgress.set(0.5)
+  }); */
+  $(document).ajaxStop(function () {
+    setTimeout(function () {
+      NProgress.done()
+    }, 1000)
+  });
 
-   })
+})
+
+// 等待页面dom结构的加载后执行
+$(function () {
+  // 注册事件完成公共功能
+  // 功能1: 左侧二级导航切换效果
+  $(".lt_aside .category").on('click', function () {
+    // alert('aaa');
+    $(".lt_aside .children").stop().slideToggle()
+  });
+   // 功能2: 左侧菜单切换效果
+  $(".icon_left").on("click", function() {
+    $(".lt_aside").toggleClass("hidemenu")
+    $('.lt_topbar').toggleClass("hidemenu");
+    $('.lt_main').toggleClass("hidemenu");
+  });
+  // 退出两种方式
+  // 1. 发ajax让后台, 销毁当前用户的登录状态, 实现退出   (推荐)
+  // 2. 清除浏览器缓存, 将cookie清空, 本地存储的 sessionId 也没了
+  // 给退出按钮, 添加点击事件, 需要在退出时, 销毁当前用户的登录状态
+  $("#logoutBtn").on ("click", function (){
+    $.ajax({
+      type: "GET",
+      url: "/employee/employeeLogout",
+      datatype: "json",
+      success: function(info) {
+        // console.log(info);
+        if (info.success) {
+          location.href = "login.html";
+        }
+      }
+    })
+  })
+})
---
+++ public/back/js/isLogin.js
@@ -0,0 +1,16 @@
+$(function () {
+  $.ajax({
+    type: "GET",
+    url: "/employee/checkRootLogin",
+    dataType: "json",
+    success: function (info) {
+      // console.log(info);
+      if (info.error === 400) {
+        location.href = "login.html"
+      }
+      if (info.success) {
+        console.log("当前用户已登陆")
+      }
+    }
+  })
+})
